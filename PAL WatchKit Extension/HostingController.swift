cb2e86658cef0ea24ab95f9d92f44083f5b27f09
sarat12us <sarat12us@gmail.com>
2020-02-21 07:19:58 -0500

    Initial Commit

---
+++ PAL WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  PAL WatchKit Extension
+//
+//  Created by Sharath Koochana on 2/21/20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ PAL/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PAL
+//
+//  Created by Sharath Koochana on 2/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
